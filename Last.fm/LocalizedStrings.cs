75c4f5166a713b624409208151f38bba0f0cffa6
alinaish <alinaish@gmail.com>
2014-02-15 20:48:50 +0400

    init

---
+++ Last.fm/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using Last.fm.Resources;
+
+namespace Last.fm
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
