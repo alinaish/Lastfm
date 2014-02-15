40aa2aa21fe49129bd1310b6e721fb1723f8215c
alinaish <alinaish@gmail.com>
2014-02-15 21:57:01 +0400

    tratata

---
+++ Last.fm/Views/Login.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace Last.fm.Views
+{
+    public partial class Login : PhoneApplicationPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
