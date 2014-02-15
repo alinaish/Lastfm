ba7e5478e613cc7c1a53b8f80ffc7551f3de0128
gerasimov <gerasimov@castor-digital.com>
2014-02-15 21:15:30 +0400

    LoginViewModel

---
+++ Last.fm/ViewModel/LoginPageViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Last.fm.ViewModel
+{
+    public class LoginPageViewModel : BaseViewModel
+    {
+        private string _login;
+        private string _password;
+
+        public string Login 
+        { 
+            get
+            {
+                return _login;
+            }
+            set 
+            { 
+                _login = value; OnPropertyChanged(); 
+            }
+        }
+
+        public string Password
+        {
+            get
+            {
+                return _password;
+            }
+            set
+            {
+                _password = value; 
+                OnPropertyChanged();
+            }
+        }
+    }
+}
